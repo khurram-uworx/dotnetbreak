51fe5ba6eb73e7d9da58676a28829fb0f3a85937
khurram-uworx <khurram.aziz@uworx.co.uk>
2024-02-06 11:48:28 +0500

    session 9

--- 0-console/Framework/Program.cs
+++ 0-console/Framework/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Messaging;
 
 namespace Framework
 {
+    public class Transaction
+    {
+        public int Account { get; set; }
+        public int Deposit { get; set; }
+        public DateTime DepositDate { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var message = new Message();
+            message.Body = new Transaction
+            {
+                Account = 10001,
+                Deposit = 500,
+                DepositDate = DateTime.Now
+            };
+
+            var queue = new MessageQueue(".\\Private$\\deposits");
+            queue.Send(message);
+
+            Console.WriteLine("Finished");
+
+            Console.ReadLine();
         }
     }
 }
